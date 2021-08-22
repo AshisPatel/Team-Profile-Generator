fc953359dc1a4e6619ca51561c12e1e00f9c312c
AP <ashis.n.patel@gmail.com>
2021-08-22 08:51:11 -0500

    Add bootstrap CDN and card HTML for manager section

--- src/page-template.js
+++ src/page-template.js
@@ -6,10 +6,17 @@ const getManager = function (teamMembers) {
 
     return managers.map(manager => {
         return `
-        ${manager.getName()} <br />
-        ${manager.getId()} <br />
-        ${manager.getEmail()} <br />
-        ${manager.getOfficeNumber()} <br />
+        <div class = "col-2 card">
+            <div class = "card-body">
+                <h5 class = "card-title">${manager.getName()}</h5>
+                <h6 class = "card-subtitle">${manager.getRole()}</h6> 
+                <ul class = "card-text list-group">
+                    <li class = "list-group-item">${manager.getId()}</li> 
+                    <li class = "list-group-item">${manager.getEmail()}</li>
+                    <li class = "list-group-item"> ${manager.getOfficeNumber()}</li>
+                </ul>
+            </div>
+        </div>
         `;
     })
 }
@@ -43,18 +50,26 @@ const getInterns = function (teamMembers) {
 }
 
 
-const generatePage = (templateData) => {
+const generatePage = (teamData) => {
     return `
     <!DOCTYPE html>
     <html lang="en">
     <head>
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
         <title>Team Builder</title>
     </head>
     <body>
-        
+        <main class="container">
+            <div class="flex-row">
+                ${getManager(teamData)}
+
+                ${getEngineers(teamData)}
+
+                ${getInterns(teamData)}
+            </div>
+        </main>
     </body>
     </html>
     `;
